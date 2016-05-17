d9f775888e1dd8dbb71c94e303ddcae99262094f
vroxo <vitor.icnv@gmail.com>
2016-05-17 11:42:34 -0300

    Criando banco e exibindo perfil pelo id

--- perfis/views.py
+++ perfis/views.py
@@ -5,9 +5,5 @@ def index(request):
 	return render(request, 'index.html')
 
 def exibir(request,perfil_id):
-	perfil = Perfil()
-
-	if (perfil_id == '1'):
-		perfil = Perfil(nome='Flavio Almeida', email='flavio@flavio.com.br', telefone='777777', nome_empresa='Caelum')
-
+	perfil = Perfil.objects.get(id=perfil_id)
 	return render(request, 'perfil.html', {'perfil': perfil})
